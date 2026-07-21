d74ecc4d14336aaab5848050c57aeb8b2bd11f0a
Thinh <thinhnguyenhuu50@gmail.com>
2026-07-21 14:50:44 +0700

    remove custom lvgl example

--- App/example/lvgl_example.h
+++
@@ -1,9 +0,0 @@
-#include "lvgl.h"
-#include "lcd.h"
-#include "touch.h"
-#include "cmsis_os.h"
-#include "task.h"
-
-void my_disp_flush(lv_display_t * disp, const lv_area_t * area, uint8_t * px_map);
-void my_touchpad_read(lv_indev_t * indev, lv_indev_data_t * data);
-void btn_event_cb(lv_event_t * e);
